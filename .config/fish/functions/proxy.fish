29c9eb3413efb0a9146dd88c2abe6edf9679ea37
Arad Alvand <aradalvand@gmail.com>
2024-12-27 15:59:49 +0330

    Robustify the `proxy` function

--- .config/fish/functions/proxy.fish
+++ .config/fish/functions/proxy.fish
@@ -5,16 +5,25 @@ function proxy --description "Manage proxy settings and whitelist IPs"
         return 1
     end
 
+    function _reset
+        if test -e "/tmp/gost-proxy.pid"
+            sudo kill -s TERM $(cat /tmp/gost-proxy.pid)
+        end
+    end
+
     set subcommand $argv[1]
 
     switch $subcommand
         case set
-            # Start proxy
+            _reset
+
+            # NOTE: We use gost to effectively transform a SOCKS proxy as an HTTP proxy, even though we coulud've done `http_proxy="socks5://37.32.12.91:443"`, because SOCKS itself is not supported by all programs that read `$http_proxy`. See https://askubuntu.com/a/1451781/1573432
             gost -L=http://:8080 -F=socks5://37.32.12.91:443 > /dev/null 2>&1 &
+            # NOTE: So that the process outlasts the shell instance (this script)
+            disown
 
+            echo $last_pid > /tmp/gost-proxy.pid
             echo "Gost process started."
-
-            echo $last_pid > $HOME/.gost-process-id
 
             export http_proxy="http://localhost:8080"
             export https_proxy="http://localhost:8080"
@@ -22,12 +31,11 @@ function proxy --description "Manage proxy settings and whitelist IPs"
             echo "Proxy set."
 
         case unset
-            # Stop proxy
-            set gost_process_id (cat $HOME/.gost-process-id)
-
-            sudo kill -s TERM $gost_process_id
+            _reset
 
             echo "Stopped Gost process."
+
+            rm /tmp/gost-proxy.pid
 
             set -e http_proxy
             set -e https_proxy
@@ -35,7 +43,6 @@ function proxy --description "Manage proxy settings and whitelist IPs"
             echo "Proxy unset."
 
         case whitelist
-            # Whitelist current IP
             echo "Fetching public IP address..."
 
             set HOST_IP (curl --noproxy '*' -s ifconfig.co)
@@ -50,7 +57,7 @@ function proxy --description "Manage proxy settings and whitelist IPs"
             set REMOTE_USER "ubuntu"
             set REMOTE_HOST "37.32.12.91"
 
-            echo "SSHing into $REMOTE_USER@$REMOTE_HOST..."
+            echo "SSH'ing into $REMOTE_USER@$REMOTE_HOST..."
 
             ssh $REMOTE_USER@$REMOTE_HOST "sudo ufw allow from $HOST_IP to any port 443 proto tcp"
 
